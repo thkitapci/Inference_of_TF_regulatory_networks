b152a0a411eb2690012cd826106b740656f9fa1b
nohaosman <osman.noha@yahoo.com>
2016-04-20 11:55:56 -0700

    Update Correlation.r

--- Correlation.r
+++ Correlation.r
@@ -1,10 +1,7 @@
-# correlation between 14 TFs and 7805 genes
+# correlation between tfselected.txt (397 TFs) and 7805 genes (distribution of input files is normal )
 
 gene=read.table("rpkm59_exp50_genes_least_50_samples.tab",sep="\t",header=T,row.names=1)
 
-
-
-# tfselected.txt is generated 
 tf=read.table("tfselected.txt",header=T,sep="\t",,row.names=1)
 
 cor_genes_TF=cor(t(gene), t(tf))
