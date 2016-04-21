80542de3c648a746d9450e1f94f79f55fe867010
nohaosman <osman.noha@yahoo.com>
2016-04-21 12:17:45 -0700

    Update Correlation.r

--- Correlation.r
+++ Correlation.r
@@ -4,7 +4,7 @@ gene=read.table("rpkm59_exp50_genes_least_50_samples.tab",sep="\t",header=T,row.
 
 tf=read.table("tfselected.txt",header=T,sep="\t",,row.names=1)
 
-cor_genes_TF=cor(t(gene), t(tf))
+cor_genes_TF=cor(t(gene), t(tf) ,method='spearman')
 
 write.table(cor_genes_TF,file="cor_tf_gene.tab", sep="\t",quote=F)
 
