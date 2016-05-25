db457b295cd87305b6967d58a491f5a11254be09
nohaosman <osman.noha@yahoo.com>
2016-05-25 15:50:05 -0700

    Update expression_Level_of_TFs.r

--- expression_Level_of_TFs.r
+++ expression_Level_of_TFs.r
@@ -1,6 +1,6 @@
 #Inputs are embryoCountsresult_with_length.txt and TF_names397.txt was obtained from FlyTF.org 
 
-#Output is tfselected.txt -> Expression level of TFs which expressed in our data
+#Output is tfselected.txt -> Expression level of TFs expressed in our data
 
 data=read.table("embryoCountsresult_with_length.txt",sep="\t",header=T)
 
